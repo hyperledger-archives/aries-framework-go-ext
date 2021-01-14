a16ac6dbd08fb639c1ffe431434a0b3c34a54d96
Derek Trider <derek.trider@securekey.com>
2021-01-14 11:21:05 -0500

    feat: Query method for new CouchDB storage implementation
    Signed-off-by: Derek Trider <Derek.Trider@securekey.com>

--- test/component/newstorage/newstore.go
+++ test/component/newstorage/newstore.go
@@ -26,6 +26,9 @@ func TestAll(t *testing.T, provider newstorage.Provider) {
 	t.Run("Delete", func(t *testing.T) {
 		TestDelete(t, provider)
 	})
+	t.Run("Query", func(t *testing.T) {
+		TestQuery(t, provider)
+	})
 }
 
 // TestPutGet tests common Put and Get functionality.
@@ -122,6 +125,295 @@ func TestDelete(t *testing.T, provider newstorage.Provider) {
 	require.Error(t, err)
 }
 
+// TestQuery tests common Query functionality.
+func TestQuery(t *testing.T, provider newstorage.Provider) { // nolint: funlen // Test file
+	t.Run("Success - tag name only query - 2 values found", func(t *testing.T) {
+		keysToPut := []string{"key1", "key2", "key3"}
+		valuesToPut := [][]byte{[]byte("value1"), []byte("value2"), []byte("value3")}
+		tagsToPut := [][]newstorage.Tag{
+			{{Name: "tagName1", Value: "tagValue1"}, {Name: "tagName2", Value: "tagValue2"}},
+			{{Name: "tagName3", Value: "tagValue"}, {Name: "tagName4"}},
+			{{Name: "tagName3", Value: "tagValue2"}},
+		}
+
+		expectedKeys := []string{keysToPut[1], keysToPut[2]}
+		expectedValues := [][]byte{valuesToPut[1], valuesToPut[2]}
+		expectedTags := [][]newstorage.Tag{tagsToPut[1], tagsToPut[2]}
+
+		queryExpression := "tagName3"
+
+		t.Run("Default page setting", func(t *testing.T) {
+			storeName := randomStoreName()
+
+			store, err := provider.OpenStore(storeName)
+			require.NoError(t, err)
+			require.NotNil(t, store)
+
+			err = provider.SetStoreConfig(storeName,
+				newstorage.StoreConfiguration{TagNames: []string{"tagName1", "tagName2", "tagName3", "tagName4"}})
+			require.NoError(t, err)
+
+			putData(t, store, keysToPut, valuesToPut, tagsToPut)
+
+			iterator, err := store.Query(queryExpression)
+			require.NoError(t, err)
+
+			verifyExpectedIterator(t, iterator, expectedKeys, expectedValues, expectedTags)
+		})
+		t.Run("Page size 2", func(t *testing.T) {
+			storeName := randomStoreName()
+
+			store, err := provider.OpenStore(storeName)
+			require.NoError(t, err)
+			require.NotNil(t, store)
+
+			err = provider.SetStoreConfig(storeName,
+				newstorage.StoreConfiguration{TagNames: []string{"tagName1", "tagName2", "tagName3", "tagName4"}})
+			require.NoError(t, err)
+
+			putData(t, store, keysToPut, valuesToPut, tagsToPut)
+
+			//nolint:gomnd // Test file
+			iterator, err := store.Query(queryExpression, newstorage.WithPageSize(2))
+			require.NoError(t, err)
+
+			verifyExpectedIterator(t, iterator, expectedKeys, expectedValues, expectedTags)
+		})
+		t.Run("Page size 1", func(t *testing.T) {
+			storeName := randomStoreName()
+
+			store, err := provider.OpenStore(storeName)
+			require.NoError(t, err)
+			require.NotNil(t, store)
+
+			err = provider.SetStoreConfig(storeName,
+				newstorage.StoreConfiguration{TagNames: []string{"tagName1", "tagName2", "tagName3", "tagName4"}})
+			require.NoError(t, err)
+
+			putData(t, store, keysToPut, valuesToPut, tagsToPut)
+
+			iterator, err := store.Query(queryExpression, newstorage.WithPageSize(1))
+			require.NoError(t, err)
+
+			verifyExpectedIterator(t, iterator, expectedKeys, expectedValues, expectedTags)
+		})
+		t.Run("Page size 100", func(t *testing.T) {
+			storeName := randomStoreName()
+
+			store, err := provider.OpenStore(storeName)
+			require.NoError(t, err)
+			require.NotNil(t, store)
+
+			err = provider.SetStoreConfig(storeName,
+				newstorage.StoreConfiguration{TagNames: []string{"tagName1", "tagName2", "tagName3", "tagName4"}})
+			require.NoError(t, err)
+
+			putData(t, store, keysToPut, valuesToPut, tagsToPut)
+
+			//nolint:gomnd // Test file
+			iterator, err := store.Query(queryExpression, newstorage.WithPageSize(100))
+			require.NoError(t, err)
+
+			verifyExpectedIterator(t, iterator, expectedKeys, expectedValues, expectedTags)
+		})
+	})
+	t.Run("Success - tag name only query - 0 values found", func(t *testing.T) {
+		keysToPut := []string{"key1", "key2", "key3"}
+		valuesToPut := [][]byte{[]byte("value1"), []byte("value2"), []byte("value3")}
+		tagsToPut := [][]newstorage.Tag{
+			{{Name: "tagName1", Value: "tagValue1"}, {Name: "tagName2", Value: "tagValue2"}},
+			{{Name: "tagName3", Value: "tagValue"}, {Name: "tagName4"}},
+			{{Name: "tagName3", Value: "tagValue2"}},
+		}
+
+		queryExpression := "nonExistentTagName"
+
+		t.Run("Default page setting", func(t *testing.T) {
+			storeName := randomStoreName()
+
+			store, err := provider.OpenStore(storeName)
+			require.NoError(t, err)
+			require.NotNil(t, store)
+
+			err = provider.SetStoreConfig(storeName,
+				newstorage.StoreConfiguration{TagNames: []string{"tagName1", "tagName2", "tagName3", "tagName4"}})
+			require.NoError(t, err)
+
+			putData(t, store, keysToPut, valuesToPut, tagsToPut)
+
+			iterator, err := store.Query(" ")
+			require.NoError(t, err)
+
+			verifyExpectedIterator(t, iterator, nil, nil, nil)
+		})
+		t.Run("Page size 2", func(t *testing.T) {
+			storeName := randomStoreName()
+
+			store, err := provider.OpenStore(storeName)
+			require.NoError(t, err)
+			require.NotNil(t, store)
+
+			err = provider.SetStoreConfig(storeName,
+				newstorage.StoreConfiguration{TagNames: []string{"tagName1", "tagName2", "tagName3", "tagName4"}})
+			require.NoError(t, err)
+
+			putData(t, store, keysToPut, valuesToPut, tagsToPut)
+
+			//nolint:gomnd // Test file
+			iterator, err := store.Query(queryExpression, newstorage.WithPageSize(2))
+			require.NoError(t, err)
+
+			verifyExpectedIterator(t, iterator, nil, nil, nil)
+		})
+		t.Run("Page size 1", func(t *testing.T) {
+			storeName := randomStoreName()
+
+			store, err := provider.OpenStore(storeName)
+			require.NoError(t, err)
+			require.NotNil(t, store)
+
+			err = provider.SetStoreConfig(storeName,
+				newstorage.StoreConfiguration{TagNames: []string{"tagName1", "tagName2", "tagName3", "tagName4"}})
+			require.NoError(t, err)
+
+			putData(t, store, keysToPut, valuesToPut, tagsToPut)
+
+			iterator, err := store.Query(queryExpression, newstorage.WithPageSize(1))
+			require.NoError(t, err)
+
+			verifyExpectedIterator(t, iterator, nil, nil, nil)
+		})
+		t.Run("Page size 100", func(t *testing.T) {
+			storeName := randomStoreName()
+
+			store, err := provider.OpenStore(storeName)
+			require.NoError(t, err)
+			require.NotNil(t, store)
+
+			err = provider.SetStoreConfig(storeName,
+				newstorage.StoreConfiguration{TagNames: []string{"tagName1", "tagName2", "tagName3", "tagName4"}})
+			require.NoError(t, err)
+
+			putData(t, store, keysToPut, valuesToPut, tagsToPut)
+
+			//nolint:gomnd // Test file
+			iterator, err := store.Query(queryExpression, newstorage.WithPageSize(100))
+			require.NoError(t, err)
+
+			verifyExpectedIterator(t, iterator, nil, nil, nil)
+		})
+	})
+	t.Run("Success - tag name and value query - 2 values found", func(t *testing.T) {
+		keysToPut := []string{"key1", "key2", "key3", "key4"}
+		valuesToPut := [][]byte{[]byte("value1"), []byte("value2"), []byte("value3"), []byte("value4")}
+		tagsToPut := [][]newstorage.Tag{
+			{{Name: "tagName1", Value: "tagValue1"}, {Name: "tagName2", Value: "tagValue2"}},
+			{{Name: "tagName3", Value: "tagValue1"}, {Name: "tagName4"}},
+			{{Name: "tagName3", Value: "tagValue2"}},
+			{{Name: "tagName3", Value: "tagValue1"}},
+		}
+
+		expectedKeys := []string{keysToPut[1], keysToPut[3]}
+		expectedValues := [][]byte{valuesToPut[1], valuesToPut[3]}
+		expectedTags := [][]newstorage.Tag{tagsToPut[1], tagsToPut[3]}
+
+		queryExpression := "tagName3:tagValue1"
+
+		t.Run("Default page setting", func(t *testing.T) {
+			storeName := randomStoreName()
+
+			store, err := provider.OpenStore(storeName)
+			require.NoError(t, err)
+			require.NotNil(t, store)
+
+			err = provider.SetStoreConfig(storeName,
+				newstorage.StoreConfiguration{TagNames: []string{"tagName1", "tagName2", "tagName3", "tagName4"}})
+			require.NoError(t, err)
+
+			putData(t, store, keysToPut, valuesToPut, tagsToPut)
+
+			iterator, err := store.Query(queryExpression)
+			require.NoError(t, err)
+
+			verifyExpectedIterator(t, iterator, expectedKeys, expectedValues, expectedTags)
+		})
+		t.Run("Page size 2", func(t *testing.T) {
+			storeName := randomStoreName()
+
+			store, err := provider.OpenStore(storeName)
+			require.NoError(t, err)
+			require.NotNil(t, store)
+
+			err = provider.SetStoreConfig(storeName,
+				newstorage.StoreConfiguration{TagNames: []string{"tagName1", "tagName2", "tagName3", "tagName4"}})
+			require.NoError(t, err)
+
+			putData(t, store, keysToPut, valuesToPut, tagsToPut)
+
+			//nolint:gomnd // Test file
+			iterator, err := store.Query(queryExpression, newstorage.WithPageSize(2))
+			require.NoError(t, err)
+
+			verifyExpectedIterator(t, iterator, expectedKeys, expectedValues, expectedTags)
+		})
+		t.Run("Page size 1", func(t *testing.T) {
+			storeName := randomStoreName()
+
+			store, err := provider.OpenStore(storeName)
+			require.NoError(t, err)
+			require.NotNil(t, store)
+
+			err = provider.SetStoreConfig(storeName,
+				newstorage.StoreConfiguration{TagNames: []string{"tagName1", "tagName2", "tagName3", "tagName4"}})
+			require.NoError(t, err)
+
+			putData(t, store, keysToPut, valuesToPut, tagsToPut)
+
+			iterator, err := store.Query(queryExpression, newstorage.WithPageSize(1))
+			require.NoError(t, err)
+
+			verifyExpectedIterator(t, iterator, expectedKeys, expectedValues, expectedTags)
+		})
+		t.Run("Page size 100", func(t *testing.T) {
+			storeName := randomStoreName()
+
+			store, err := provider.OpenStore(storeName)
+			require.NoError(t, err)
+			require.NotNil(t, store)
+
+			err = provider.SetStoreConfig(storeName,
+				newstorage.StoreConfiguration{TagNames: []string{"tagName1", "tagName2", "tagName3", "tagName4"}})
+			require.NoError(t, err)
+
+			putData(t, store, keysToPut, valuesToPut, tagsToPut)
+
+			//nolint:gomnd // Test file
+			iterator, err := store.Query(queryExpression, newstorage.WithPageSize(100))
+			require.NoError(t, err)
+
+			verifyExpectedIterator(t, iterator, expectedKeys, expectedValues, expectedTags)
+		})
+	})
+	t.Run("Failure - invalid expression formats", func(t *testing.T) {
+		storeName := randomStoreName()
+
+		store, err := provider.OpenStore(storeName)
+		require.NoError(t, err)
+		require.NotNil(t, store)
+
+		t.Run("Blank expression", func(t *testing.T) {
+			iterator, err := store.Query("")
+			require.EqualError(t, err, "invalid expression format. it must be in the following format: TagName:TagValue")
+			require.Empty(t, iterator)
+		})
+		t.Run("Too many colon-separated parts", func(t *testing.T) {
+			iterator, err := store.Query("name:value:somethingElse")
+			require.EqualError(t, err, "invalid expression format. it must be in the following format: TagName:TagValue")
+			require.Empty(t, iterator)
+		})
+	})
+}
+
 func tryNilOrBlankValues(t *testing.T, store newstorage.Store, data []byte, commonKey string) {
 	// Try getting blank key
 	_, err := store.Get("")
@@ -138,4 +430,125 @@ func tryNilOrBlankValues(t *testing.T, store newstorage.Store, data []byte, comm
 
 func randomStoreName() string {
 	return "store-" + uuid.New().String()
+}
+
+func putData(t *testing.T, store newstorage.Store, keys []string, values [][]byte, tags [][]newstorage.Tag) {
+	for i := 0; i < len(keys); i++ {
+		err := store.Put(keys[i], values[i], tags[i]...)
+		require.NoError(t, err)
+	}
+}
+
+func verifyExpectedIterator(t *testing.T, // nolint:gocyclo,funlen // Test file
+	actualResultsItr newstorage.Iterator,
+	expectedKeys []string, expectedValues [][]byte, expectedTags [][]newstorage.Tag) {
+	if len(expectedValues) != len(expectedKeys) || len(expectedTags) != len(expectedKeys) {
+		require.FailNow(t,
+			"invalid test case. Expected keys, values and tags slices must be the same length")
+	}
+
+	var dataChecklist struct {
+		keys     []string
+		values   [][]byte
+		tags     [][]newstorage.Tag
+		received []bool
+	}
+
+	dataChecklist.keys = expectedKeys
+	dataChecklist.values = expectedValues
+	dataChecklist.tags = expectedTags
+	dataChecklist.received = make([]bool, len(expectedKeys))
+
+	moreResultsToCheck, err := actualResultsItr.Next()
+	require.NoError(t, err)
+
+	for moreResultsToCheck {
+		dataReceivedCount := 0
+
+		for _, received := range dataChecklist.received {
+			if received {
+				dataReceivedCount++
+			}
+		}
+
+		if dataReceivedCount == len(dataChecklist.received) {
+			require.FailNow(t, "query returned more results than expected")
+		}
+
+		var itrErr error
+		receivedKey, itrErr := actualResultsItr.Key()
+		require.NoError(t, itrErr)
+
+		receivedValue, itrErr := actualResultsItr.Value()
+		require.NoError(t, itrErr)
+
+		receivedTags, itrErr := actualResultsItr.Tags()
+		require.NoError(t, itrErr)
+
+		for i := 0; i < len(dataChecklist.keys); i++ {
+			if receivedKey == dataChecklist.keys[i] {
+				if string(receivedValue) == string(dataChecklist.values[i]) {
+					if equalTags(receivedTags, dataChecklist.tags[i]) {
+						dataChecklist.received[i] = true
+
+						break
+					}
+				}
+			}
+		}
+
+		moreResultsToCheck, err = actualResultsItr.Next()
+		require.NoError(t, err)
+	}
+
+	err = actualResultsItr.Release()
+	require.NoError(t, err)
+
+	for _, received := range dataChecklist.received {
+		if !received {
+			require.FailNow(t, "received unexpected query results")
+		}
+	}
+}
+
+func equalTags(tags1, tags2 []newstorage.Tag) bool { //nolint:gocyclo // Test file
+	if len(tags1) != len(tags2) {
+		return false
+	}
+
+	matchedTags1 := make([]bool, len(tags1))
+	matchedTags2 := make([]bool, len(tags2))
+
+	for i, tag1 := range tags1 {
+		for j, tag2 := range tags2 {
+			if matchedTags2[j] {
+				continue // This tag has already found a match. Tags can only have one match!
+			}
+
+			if tag1.Name == tag2.Name && tag1.Value == tag2.Value {
+				matchedTags1[i] = true
+				matchedTags2[j] = true
+
+				break
+			}
+		}
+
+		if !matchedTags1[i] {
+			return false
+		}
+	}
+
+	for _, matchedTag := range matchedTags1 {
+		if !matchedTag {
+			return false
+		}
+	}
+
+	for _, matchedTag := range matchedTags2 {
+		if !matchedTag {
+			return false
+		}
+	}
+
+	return true
 }
