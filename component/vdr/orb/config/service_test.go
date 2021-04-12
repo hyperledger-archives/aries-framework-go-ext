c08f45e61dc2bd03f9517d2c4d3e28d6f62785d1
Firas Qutishat <firas.qutishat@securekey.com>
2021-04-12 05:48:18 -0400

    feat: add discovery domain for orb vdr
    Signed-off-by: Firas Qutishat <firas.qutishat@securekey.com>

--- component/vdr/orb/config/service_test.go
+++ component/vdr/orb/config/service_test.go
@@ -7,9 +7,16 @@ SPDX-License-Identifier: Apache-2.0
 package config
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"github.com/trustbloc/orb/pkg/discovery/endpoint/restapi"
 )
 
 func TestConfigService_GetSidetreeConfig(t *testing.T) {
@@ -21,4 +28,155 @@ func TestConfigService_GetSidetreeConfig(t *testing.T) {
 
 		require.Equal(t, uint(18), conf.MultiHashAlgorithm)
 	})
+}
+
+func TestConfigService_GetEndpoint(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		cs := NewService(WithAuthToken("t1"), WithHTTPClient(
+			&mockHTTPClient{doFunc: func(req *http.Request) (*http.Response, error) {
+				if strings.Contains(req.URL.Path, ".well-known/did-orb") {
+					b, err := json.Marshal(restapi.WellKnownResponse{
+						OperationEndpoint:  "/op",
+						ResolutionEndpoint: "/resolve",
+					})
+					require.NoError(t, err)
+					r := ioutil.NopCloser(bytes.NewReader(b))
+
+					return &http.Response{StatusCode: http.StatusOK, Body: r}, nil
+				}
+
+				if strings.Contains(req.URL.Path, ".well-known/webfinger") &&
+					strings.Contains(req.URL.RawQuery, "op") {
+					b, err := json.Marshal(restapi.WebFingerResponse{
+						Links: []restapi.WebFingerLink{{Href: "/op1"}, {Href: "/op2"}},
+					})
+					require.NoError(t, err)
+					r := ioutil.NopCloser(bytes.NewReader(b))
+
+					return &http.Response{StatusCode: http.StatusOK, Body: r}, nil
+				}
+
+				if strings.Contains(req.URL.Path, ".well-known/webfinger") &&
+					strings.Contains(req.URL.RawQuery, "resolve") {
+					b, err := json.Marshal(restapi.WebFingerResponse{
+						Properties: map[string]interface{}{minResolvers: float64(2)},
+						Links:      []restapi.WebFingerLink{{Href: "/resolve1"}, {Href: "/resolve2"}},
+					})
+					require.NoError(t, err)
+					r := ioutil.NopCloser(bytes.NewReader(b))
+
+					return &http.Response{StatusCode: http.StatusOK, Body: r}, nil
+				}
+
+				return nil, nil
+			}}))
+
+		endpoint, err := cs.GetEndpoint("d1")
+		require.NoError(t, err)
+
+		require.Equal(t, endpoint.ResolutionEndpoints, []string{"/resolve1", "/resolve2"})
+		require.Equal(t, endpoint.OperationEndpoints, []string{"/op1", "/op2"})
+		require.Equal(t, endpoint.MinResolvers, 2)
+	})
+
+	t.Run("fail to send request for well-known", func(t *testing.T) {
+		cs := NewService(WithHTTPClient(
+			&mockHTTPClient{doFunc: func(req *http.Request) (*http.Response, error) {
+				return nil, fmt.Errorf("failed to send")
+			}}))
+
+		_, err := cs.GetEndpoint("d1")
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "failed to send")
+	})
+
+	t.Run("well-known return 500 status", func(t *testing.T) {
+		cs := NewService(WithHTTPClient(
+			&mockHTTPClient{doFunc: func(req *http.Request) (*http.Response, error) {
+				return &http.Response{
+					StatusCode: http.StatusInternalServerError,
+					Body:       ioutil.NopCloser(bytes.NewReader([]byte{})),
+				}, nil
+			}}))
+
+		_, err := cs.GetEndpoint("d1")
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "got unexpected response from d1/.well-known/did-orb status")
+	})
+
+	t.Run("web finger resolution return 500 status", func(t *testing.T) {
+		cs := NewService(WithHTTPClient(
+			&mockHTTPClient{doFunc: func(req *http.Request) (*http.Response, error) {
+				if strings.Contains(req.URL.Path, ".well-known/did-orb") {
+					b, err := json.Marshal(restapi.WellKnownResponse{
+						OperationEndpoint:  "/op",
+						ResolutionEndpoint: "/resolve",
+					})
+					require.NoError(t, err)
+					r := ioutil.NopCloser(bytes.NewReader(b))
+
+					return &http.Response{StatusCode: http.StatusOK, Body: r}, nil
+				}
+
+				return &http.Response{
+					StatusCode: http.StatusInternalServerError,
+					Body:       ioutil.NopCloser(bytes.NewReader([]byte{})),
+				}, nil
+			}}))
+
+		_, err := cs.GetEndpoint("d1")
+		require.Error(t, err)
+		require.Contains(t, err.Error(),
+			"got unexpected response from d1/.well-known/webfinger?resource=%2Fresolve status")
+	})
+
+	t.Run("web finger operation return 500 status", func(t *testing.T) {
+		cs := NewService(WithHTTPClient(
+			&mockHTTPClient{doFunc: func(req *http.Request) (*http.Response, error) {
+				if strings.Contains(req.URL.Path, ".well-known/did-orb") {
+					b, err := json.Marshal(restapi.WellKnownResponse{
+						OperationEndpoint:  "/op",
+						ResolutionEndpoint: "/resolve",
+					})
+					require.NoError(t, err)
+					r := ioutil.NopCloser(bytes.NewReader(b))
+
+					return &http.Response{StatusCode: http.StatusOK, Body: r}, nil
+				}
+
+				if strings.Contains(req.URL.Path, ".well-known/webfinger") &&
+					strings.Contains(req.URL.RawQuery, "resolve") {
+					b, err := json.Marshal(restapi.WebFingerResponse{
+						Properties: map[string]interface{}{minResolvers: float64(2)},
+						Links:      []restapi.WebFingerLink{{Href: "/resolve1"}, {Href: "/resolve2"}},
+					})
+					require.NoError(t, err)
+					r := ioutil.NopCloser(bytes.NewReader(b))
+
+					return &http.Response{StatusCode: http.StatusOK, Body: r}, nil
+				}
+
+				return &http.Response{
+					StatusCode: http.StatusInternalServerError,
+					Body:       ioutil.NopCloser(bytes.NewReader([]byte{})),
+				}, nil
+			}}))
+
+		_, err := cs.GetEndpoint("d1")
+		require.Error(t, err)
+		require.Contains(t, err.Error(),
+			"got unexpected response from d1/.well-known/webfinger?resource=%2Fop status")
+	})
+}
+
+type mockHTTPClient struct {
+	doFunc func(req *http.Request) (*http.Response, error)
+}
+
+func (m *mockHTTPClient) Do(req *http.Request) (*http.Response, error) {
+	if m.doFunc != nil {
+		return m.doFunc(req)
+	}
+
+	return nil, nil
 }
--- component/vdr/orb/go.mod
+++ component/vdr/orb/go.mod
@@ -10,4 +10,5 @@ require (
 	github.com/hyperledger/aries-framework-go v0.1.7-0.20210330153939-7ec3a2c4697c
 	github.com/hyperledger/aries-framework-go-ext/component/vdr/sidetree v0.0.0-20210330163233-9482b4291d8e
 	github.com/stretchr/testify v1.7.0
+	github.com/trustbloc/orb v0.0.0-20210410015721-45a365fa46a2
 )
