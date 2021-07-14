cfca30ed050638aa4acfa1bb7391e3294f9c970d
Andrii Soluk <isoluchok@gmail.com>
2021-07-14 13:52:09 +0300

    fix: Fixes bdd tests
    Signed-off-by: Andrii Soluk <isoluchok@gmail.com>

--- test/bdd/vdr/orb/bddtests_test.go
+++ test/bdd/vdr/orb/bddtests_test.go
@@ -68,7 +68,7 @@ func runBDDTests(tags, format string) int { //nolint: gocognit
 					composition = append(composition, newComposition)
 				}
 				fmt.Println("docker-compose up ... waiting for containers to start ...")
-				testSleep := 15
+				testSleep := 40
 				if os.Getenv("TEST_SLEEP") != "" {
 					var e error
 
